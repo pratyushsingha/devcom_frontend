710140aee0fdd0ad853cd9623f944938abf314cf
pratyushsingha <pratyushsingha83@gmail.com>
2024-01-13 19:54:21 +0530

    fix: fixed the private route bug

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,35 @@
-import { createContext, useState } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 export default function AuthContextProvider({ children }) {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const refreshAccessToken = async () => {
+        const refreshToken = localStorage.getItem("refreshToken");
+        try {
+          const response = await axios.post(
+            "/users/refresh-token",
+            refreshToken,
+            { withCredentials: true }
+          );
+          const accessToken = localStorage.setItem("accessToken", accessToken);
+          const refreshToken = localStorage.setItem(
+            "refreshToken",
+            refreshToken
+          );
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      refreshAccessToken();
+    }, 30000);
+    return clearInterval(interval);
+  }, []);
+
   const values = { auth, setAuth };
   return (
     <AuthContext.Provider value={values}>{children} </AuthContext.Provider>
